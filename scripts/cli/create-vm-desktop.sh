c4f37023f3e4b33796460e03047c2725afdc07f6
Bruce Mundin <bruce.mundin@ipl.com>
2018-06-08 14:18:28 +0100

    Simple desktop
    Create Simple

--- scripts/cli/create-vm-desktop.sh
+++ scripts/cli/create-vm-desktop.sh
@@ -1,7 +1,7 @@
 export project_name="Dummy"
 export region="UKWest"
 export resourceGroup="${project_name}ResourceGroup"
-
+cre
 # echo Creating machineName "${USER}-$1" 
 # echo in resourceGroup "${resourceGroup}"
 # echo With password "$2"
@@ -10,4 +10,4 @@ export resourceGroup="${project_name}ResourceGroup"
 # Create VM
 echo az vm create --verbose --resource-group $resourceGroup --name ${USER}-$1 --image UbuntuLTS --admin-username $USER --admin-password $2 --size Standard_D2_v2 
 # Run extensions to set up VM
-echo az vm extension set --resource-group $resourceGroup --vm-name ${USER}-$1 --name CustomScript --publisher Microsoft.Azure.Extensions --version 2.0 --protected-settings ./create-vm-simple.json
+echo az vm extension set --resource-group $resourceGroup --vm-name ${USER}-$1 --name CustomScript --publisher Microsoft.Azure.Extensions --version 2.0 --protected-settings ./create-vm-desktop.json
