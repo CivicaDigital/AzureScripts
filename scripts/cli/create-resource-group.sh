64b49cb87bcf8f13cfd23b9f9d1b110318e013a1
Bruce Mundin <bruce.mundin@ipl.com>
2018-06-12 12:23:53 +0100

    Linux line endings.

--- scripts/cli/create-resource-group.sh
+++ scripts/cli/create-resource-group.sh
@@ -1,4 +1,7 @@
-# Create the resource group
+#!/bin/bash
+source variables.sh
+echo Creating the resource group
 az group create -n $resourceGroup -l $region
+
 # Add tags to the resource group
 az group update -n $resourceGroup --set tags.CostCenter='{"Dept":"IT","Environment":"Test"}'
---
+++ scripts/cli/variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo Setting Variables
+
+if [ -z "$1" ]; then echo "\$1 is empty needed for 'project_name'"; exit ; fi
+
+# Variables for All scripts, copy and paste into the command line.
+export project_name="$1"
+export region="UKWest"
+export resourceGroup="${project_name}ResourceGroup"
+echo Working in resourceGroup ${resourceGroup} in region ${region}.
