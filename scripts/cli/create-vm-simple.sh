a807990803d0172ff9f0d9b8e8ef3353f745b668
Bruce Mundin <bruce.mundin@ipl.com>
2018-06-12 15:01:46 +0100

    fix?

--- scripts/cli/create-vm-simple.sh
+++ scripts/cli/create-vm-simple.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source variables.sh $1
-if [ !-n "$2" ]; then echo "\$2 is empty needed for 'machineName'"; exit ; fi
-if [ !-n "$3" ]; then echo "\$3 is empty needed for 'admin-password'"; exit ; fi
-if [ !-n "$4" ]; then export vm_init_settings="./create-vm-simple.json" ; else export vm_init_settings=$4 ;  fi
+if [ ! -n "$2" ]; then echo "\$2 is empty needed for 'machineName'"; exit ; fi
+if [ ! -n "$3" ]; then echo "\$3 is empty needed for 'admin-password'"; exit ; fi
+if [ ! -n "$4" ]; then export vm_init_settings="./create-vm-simple.json" ; else export vm_init_settings=$4 ;  fi
 
 source create-resource-group.s
 echo Creating machineName ${USER}-$2 
