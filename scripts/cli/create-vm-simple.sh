f0918680613558fd5377a18e0632cd1e301a9055
Bruce Mundin <bruce.mundin@ipl.com>
2018-06-12 14:13:38 +0100

    Params wrong

--- scripts/cli/create-vm-simple.sh
+++ scripts/cli/create-vm-simple.sh
@@ -7,9 +7,9 @@ source create-resource-group.sh
 echo Creating machineName ${USER}-$2 
 echo in resourceGroup ${resourceGroup}
 echo With password $2
-echo az vm create --verbose --resource-group $resourceGroup --name ${USER}-$2 --image UbuntuLTS --admin-username $USER --admin-password $2 --size Standard_D2_v2 
+az vm create --verbose --resource-group $resourceGroup --name ${USER}-$2 --image UbuntuLTS --admin-username $USER --admin-password $2 --size Standard_D2_v2 
 
-source variables.sh
+source wait-for-vm.sh
 
 # Run extensions to set up VM
-echo az vm extension set --resource-group $resourceGroup --vm-name ${USER}-$2 --name CustomScript --publisher Microsoft.Azure.Extensions --version 2.0 --protected-settings ./create-vm-simple.json
+az vm extension set --resource-group $resourceGroup --vm-name ${USER}-$2 --name CustomScript --publisher Microsoft.Azure.Extensions --version 2.0 --protected-settings ./create-vm-simple.json
