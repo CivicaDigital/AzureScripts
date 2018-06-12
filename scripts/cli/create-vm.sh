ebc667d5d61f2fb5c77b1912bd45080bab356787
Bruce Mundin <bruce.mundin@ipl.com>
2018-06-12 15:11:41 +0100

    Fix

--- scripts/cli/create-vm.sh
+++ scripts/cli/create-vm.sh
@@ -4,7 +4,8 @@ if [ -z "$2" ]; then echo "\$2 is empty needed for 'machineName'"; exit ; fi
 if [ -z "$3" ]; then echo "\$3 is empty needed for 'admin-password'"; exit ; fi
 if [ -z "$4" ]; then export vm_init_settings="./create-vm-simple.json" ; else export vm_init_settings=$4 ;  fi
 
-source create-resource-group.s
+source create-resource-group.sh
+
 echo Creating machineName ${USER}-$2 
 echo in resourceGroup ${resourceGroup}
 echo With password $3
