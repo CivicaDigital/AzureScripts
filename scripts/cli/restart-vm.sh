ff64c6fb87d496b2841a870ccb055d8a1c744932
Bruce Mundin <bruce.mundin@ipl.com>
2018-06-12 16:58:00 +0100

    echo output

--- scripts/cli/restart-vm.sh
+++ scripts/cli/restart-vm.sh
@@ -1,2 +1,5 @@
-# Stops all machines (vms) in Test
+echo Restarts all machines on account with 'restart-tag'.
+echo ---- LIST -----------------------------
+az resource list --tag "restart-tag" --query "[?type=='Microsoft.Compute/virtualMachines'].name
+echo ---------------------------------------
 az vm restart --ids $(az resource list --tag "restart-tag" --query "[?type=='Microsoft.Compute/virtualMachines'].id" -o tsv)
--- scripts/cli/stop-vm.sh
+++ scripts/cli/stop-vm.sh
@@ -1,2 +1,5 @@
-# Stops all machines (vms) in Test
+echo Stops all machines on account with 'restart-tag'.
+echo ---- LIST -----------------------------
+az resource list --tag "restart-tag" --query "[?type=='Microsoft.Compute/virtualMachines'].name
+echo ---------------------------------------
 az vm stop --ids $(az resource list --tag "restart-tag" --query "[?type=='Microsoft.Compute/virtualMachines'].id" -o tsv)
