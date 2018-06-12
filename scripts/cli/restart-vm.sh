8574ef840b4ce1fe98e6be24d62b6f3aa0a4423c
Bruce Mundin <bruce.mundin@ipl.com>
2018-06-12 16:48:14 +0100

    start & stop

---
+++ scripts/cli/restart-vm.sh
@@ -0,0 +1,2 @@
+# Stops all machines (vms) in Test
+az vm restart --ids $(az resource list --tag "restart-tag" --query "[?type=='Microsoft.Compute/virtualMachines'].id" -o tsv)
--- scripts/cli/stop-vm.sh
+++ scripts/cli/stop-vm.sh
@@ -1,2 +1,2 @@
 # Stops all machines (vms) in Test
-az vm stop --ids $(az resource list --tag Environment=Test --query "[?type=='Microsoft.Compute/virtualMachines'].id" --output tsv)
+az vm stop --ids $(az resource list --tag "restart-tag" --query "[?type=='Microsoft.Compute/virtualMachines'].id" -o tsv)
