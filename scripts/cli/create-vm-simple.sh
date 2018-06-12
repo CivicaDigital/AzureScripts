9dfd897ef4306045b927b8e5ecf73e86ffda7c7c
Bruce Mundin <bruce.mundin@ipl.com>
2018-06-12 12:46:51 +0100

    use create-resource-group.sh

--- scripts/cli/create-vm-simple.sh
+++ scripts/cli/create-vm-simple.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-source variables.sh
-echo Creating the resource group
-az group create -n $resourceGroup -l $region
+source variables.sh $1
 
-echo Creating machineName ${USER}-$1 
+source create-resource-group.sh
+
+if [ -z "$1" ]; then echo "\$1 is empty needed for 'project_name'"; exit ; fi
+
+echo Creating machineName ${USER}-$2 
 echo in resourceGroup ${resourceGroup}
 echo With password $2
-echo az vm create --verbose --resource-group $resourceGroup --name ${USER}-$1 --image UbuntuLTS --admin-username $USER --admin-password $2 --size Standard_D2_v2 
+echo az vm create --verbose --resource-group $resourceGroup --name ${USER}-$2 --image UbuntuLTS --admin-username $USER --admin-password $2 --size Standard_D2_v2 
 
 source variables.sh
 
 # Run extensions to set up VM
-echo az vm extension set --resource-group $resourceGroup --vm-name ${USER}-$1 --name CustomScript --publisher Microsoft.Azure.Extensions --version 2.0 --protected-settings ./create-vm-simple.json
+echo az vm extension set --resource-group $resourceGroup --vm-name ${USER}-$2 --name CustomScript --publisher Microsoft.Azure.Extensions --version 2.0 --protected-settings ./create-vm-simple.json
