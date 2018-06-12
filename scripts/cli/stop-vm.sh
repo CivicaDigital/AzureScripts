cb20b3f15a5f5b92f69a05628125cc9ce04054f0
Bruce Mundin <bruce.mundin@ipl.com>
2018-06-12 16:59:59 +0100

    fix

--- scripts/cli/stop-vm.sh
+++ scripts/cli/stop-vm.sh
@@ -1,5 +1,5 @@
 echo Stops all machines on account with 'restart-tag'.
 echo ---- LIST -----------------------------
-az resource list --tag "restart-tag" --query "[?type=='Microsoft.Compute/virtualMachines'].name
+az resource list --tag "restart-tag" --query "[?type=='Microsoft.Compute/virtualMachines'].name"
 echo ---------------------------------------
 az vm stop --ids $(az resource list --tag "restart-tag" --query "[?type=='Microsoft.Compute/virtualMachines'].id" -o tsv)
