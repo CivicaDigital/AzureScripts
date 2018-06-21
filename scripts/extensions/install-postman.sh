e72ae5930b83cda11085d89f4f07299f2823eefd
Bruce Mundin <bruce.mundin@ipl.com>
2018-06-21 11:53:19 +0100

    postman update

--- scripts/extensions/install-postman.sh
+++ scripts/extensions/install-postman.sh
@@ -7,11 +7,13 @@ sudo echo $(date) ":Installing Postman" >> /usr/civica/cloud-init.txt
 
 cd ~
 # Install this package to get required libraries
-sudo yum -y -q install libXScrnSaver
+sudo apt-get install libxss1
 wget -q https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz
 sudo tar -xzf postman.tar.gz -C /opt
 rm postman.tar.gz
 sudo ln -s /opt/Postman/Postman /usr/bin/postman
+
+
 sudo mv /tmp/postman.desktop /usr/share/applications
 
 sudo echo $(date) ":Installed Postman" >> /usr/civica/cloud-init.txt
